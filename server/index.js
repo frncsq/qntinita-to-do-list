436f75c25e4125eff0db50c83e4592911ed5f7b1
Flight <quintinitafrancismari@gmail.com>
2026-01-23 08:41:39 +0800

    add: register

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import session from 'express-session';
 import { pool } from './db.js';
+import { hashPassword, comparePassword } from './components/hash.js';
+
+
 const app = express();
 app.use(express.json());
+app.use(session({
+  secret: '1234567890', 
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } 
+}));
 
 
 const PORT = 3000;
@@ -103,6 +113,82 @@ app. post('/delete-item', async  (req, res) => {
 });
 
 
-app.listen (PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+
+app.post('/register', async (req, res) => {
+  const {username, password, confirm, name } = req.body;
+  if(password === confirm ){
+    try {
+      const hashedPassword = await hashPassword(password);
+      await pool.query(
+       "INSERT INTO user_accounts (username, password, name) VALUES ($1, $2, $3)", [username, hashedPassword, name]
+      );
+      res.status(200).json({ success: true, message: "Registered successfully"});
+    } catch (error) {
+      res.status(500).json({ success: false, message: "Error registering user" });
+    }
+  } else {
+    res.status(401).json({ success: false, message: "Handa agparis"});
+  
+}
 });
+app.post('/login', async (req, res) => {
+  const { username, name, password } = req.body;
+  try {
+    const result = await pool.query(
+      "SELECT * FROM user_accounts WHERE name = $1", [name]
+    );
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const isPasswordValid = await comparePassword(password, user.password);
+      if (isPasswordValid) {
+        req.session.userId = user.id;
+        req.session.name = user.name;
+        res.status(200).json({ success: true, message: "Login successfully" });
+      } else {
+        res.status(401).json({ success: false, message: "Invalid username or password" });
+      }
+    } else {
+      res.status(401).json({ success: false, message: "Invalid username or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error logging in" });
+  }
+});
+
+app.get('/get-session', (req, res) => {
+  if (req.session.userId) {
+    res.status(200).json({
+      session: true,
+      userId: req.session.userId,
+      name: req.session.name
+    });
+  } else {
+    res.status(200).json({ session: false });
+  }
+});
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).json({ success: false, message: "Error logging out" });
+    } else {
+      res.status(200).json({ success: true, message: "Logged out successfully" });
+    }
+  });
+}); 
+app.listen (PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
