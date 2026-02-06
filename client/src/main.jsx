e398622d6fd295d942b61164e632dde9b45a0d41
Flight <francismariquintinita@gmail.com>
2026-02-06 11:07:12 +0800

    fix route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/home.jsx'
-import Register from './pages/register.jsx'
-import ListItem from './pages/list-item.jsx'
-import './css/globals.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import Home from './pages/home.jsx';
+import Register from './pages/register.jsx';
+import ListItem from './pages/list-item.jsx';
+import './css/globals.css';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/ListItem" element={<ListItem />} />
-        
+        {/* App is now the layout for nested pages */}
+        <Route path="*" element={<App />}>
+          <Route index element={<Home />} />            {/* Default page "/" */}
+          <Route path="Home" element={<Home />} />      {/* "/Home" */}
+          <Route path="Register" element={<Register />} />  {/* "/Register" */}
+          <Route path="ListItem" element={<ListItem />} />  {/* "/ListItem" */}
+        </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
