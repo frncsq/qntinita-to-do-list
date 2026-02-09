a07d3fd24b1f4e4437c115348a8fb57d29388f06
Flight <francismariquintinita@gmail.com>
2026-02-09 20:32:42 +0800

    add cors link

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: [
     
     "https://qntinita-to-do-list-git-main-rises-projects-b13889a1.vercel.app/",
+    "https://qntinita-to-do-list-dzay7f8xu-rises-projects-b13889a1.vercel.app"
     
   ],
   credentials: true,
